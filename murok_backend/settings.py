6f97a270ed98e15f804054544f3aa0a5a64b51a6
Capella87 <contact@capelladev.com>
2023-10-15 02:56:06 +0900

    Add reading SECRET_KEY from file

--- murok_backend/settings.py
+++ murok_backend/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+
+
+def get_secrets(filename: str) -> str:
+    filename = BASE_DIR / filename
+    with open(filename, 'r') as f:
+        secrets = json.loads(f.read())
+
+    return secrets.get('SECRET_KEY', '')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =
+SECRET_KEY = get_secrets('secrets.json')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
