67cc5d954fa132889dacc470cc6ef4f9a896c61d
LeeDayday <dayoon0905@gmail.com>
2023-10-25 00:41:42 +0900

    feat: Add admin.py for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'name','is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
