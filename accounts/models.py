02e5d2d475f8f16f11744beaf3ac84b7f453c3fe
LeeDayday <dayoon0905@gmail.com>
2023-10-25 00:40:58 +0900

    FIX: Fix UserManager to automatically generate UUID for users

--- accounts/models.py
+++ accounts/models.py
@@ -4,24 +4,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, uuid, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, uuid=uuid)
+        user = self.model(email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, uuid, password=None):
-        user = self.create_user(email, uuid, password)
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, blank=False)
     name = models.CharField(max_length=50, default='', blank=True)
     is_active = models.BooleanField(default=True)
