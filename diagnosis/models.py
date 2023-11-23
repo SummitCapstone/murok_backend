f814a5e970fbb54849704a21eb3693852bbd162e
Capella87 <contact@capelladev.com>
2023-11-23 16:10:28 +0900

    Change CropCategory to models.TextChoices

--- diagnosis/models.py
+++ diagnosis/models.py
@@ -4,14 +4,22 @@ from accounts.models import User, RequestUser
 
 
 class UserDiagnosisRequest(models.Model):
+    class CropCategory(models.TextChoices):
+        UNKNOWN = 'UNKNOWN'
+        STRAWBERRY = 'STRAWBERRY'
+        TOMATO = 'TOMATO'
+        PEPPER = 'PEPPER'
+        CUCUMBER = 'CUCUMBER'
+
+
     id = models.UUIDField(primary_key=True,
                           editable=False)
     picture = models.ImageField(upload_to='static/uploads/blob/%Y/%m/%d/',
                                 blank=False, null=False)
     crop_category = models.CharField(max_length=50,
-                                     default='UNKNOWN',
+                                     default=CropCategory.UNKNOWN,
                                      blank=False,
-                                     null=False)
+                                     choices=CropCategory.choices)
     request_date = models.DateTimeField(auto_now_add=True,
                                         editable=False)
     request_user_id = models.ForeignKey(RequestUser,
